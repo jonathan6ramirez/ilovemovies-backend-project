21a152866217e26cc80b1357182184e123beba5e
JonathanRamirez <ramirezjonathan21@gmail.com>
2021-12-14 16:02:03 -0600

    Defined the table for the movies_theaters migration as well as the rollback.

--- src/db/migrations/20211214145940_createReviews.js
+++ src/db/migrations/20211214145940_createReviews.js
@@ -14,6 +14,8 @@ exports.up = function(knex) {
             .references("movie_id")
             .inTable("movies")
             .onDelete("CASCADE")
+
+        table.timestamps(true, true);
     })
 };
 
