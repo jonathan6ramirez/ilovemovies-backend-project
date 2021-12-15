0bde9cde6a059bff71fc0edec535dc830e921b37
JonathanRamirez <ramirezjonathan21@gmail.com>
2021-12-14 20:18:49 -0600

    Added the column inside the createMovies migration.

--- src/db/migrations/20211214180825_createMovies.js
+++ src/db/migrations/20211214180825_createMovies.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     return knex.schema.createTable("movies", (table) => {
         table.increments("movie_id").primary();
         table.string("title");
+        table.integer("runtime_in_minutes").unsigned().notNullable();
         table.string("rating");
         table.text("description");
         table.string("image_url");
