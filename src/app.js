61c3b135ab9b7da4e1c45031b43893a4cb3fb8fc
JonathanRamirez <ramirezjonathan21@gmail.com>
2021-12-15 00:53:42 -0600

    Added the route for the GET method to /theaters and enabled CORS as well. I only need to fix the data object returned.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,26 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+//Theaters, movies, and reviews routers
+const theatersRouter = require("./theaters/theaters.router");
+
+app.use(express.json());
+
+app.use("/theaters", theatersRouter);
+
+//Not found handler
+app.use((req, res, next) => {
+    next({
+        status: 404,
+        message: `Not found ${req.originalUrl}`,
+    });
+})
+
+//Error handler
+app.use((error, req, res, next) => {
+    console.log(error);
+    const { status = 500, message = `Something went wrong!`} = error;
+    res.status(status).json({ error: message });
+})
+
 module.exports = app;
--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,3 +1,13 @@
-const router = require("express");
+const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+const corsGet = cors({ methods: "GET" })
+
+router.route("/")
+    .get(corsGet ,controller.list)
+    .options(corsGet)
+    .all(methodNotAllowed);
+
+module.exports = router;
